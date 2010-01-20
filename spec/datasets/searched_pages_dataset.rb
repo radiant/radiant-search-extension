48cfac597aa6e59000ae92d4a958c010f07524ae
Vitali <vint@bestia.yakovenko.net>
2010-01-20 00:12:17 +0000

    Fix search_page_dataset initialization. Issue #5

--- spec/datasets/searched_pages_dataset.rb
+++ spec/datasets/searched_pages_dataset.rb
@@ -3,10 +3,10 @@ class SearchedPagesDataset < Dataset::Base
   
   def load
     create_page "Ruby Home Page" do
-      create_page_part 'body', :content => 'This is the body portion of the Ruby home page.'
-      create_page_part 'extended', :content => 'This is an extended portion of the Ruby home page.'
-      create_page_part 'summary', :content => 'This is a summary.'
-      create_page_part 'sidebar', :content => '<r:title /> sidebar.'
+      create_page_part :ruby_home_page_body, :content => 'This is the body portion of the Ruby home page.'
+      create_page_part :ruby_home_page_extended, :content => 'This is an extended portion of the Ruby home page.'
+      create_page_part :ruby_home_page_summary, :content => 'This is a summary.'
+      create_page_part :ruby_home_page_sidebar, :content => '<r:title /> sidebar.'
     end
     create_page "Documentation", :body => 'This is the documentation section.'
     create_page "Search", :body => "This is the search section.", :class_name => 'SearchPage'
--- spec/models/search_page_spec.rb
+++ spec/models/search_page_spec.rb
@@ -4,7 +4,7 @@ describe SearchPage do
   dataset :searched_pages
   describe "<r:truncate_and_strip>" do
     it "should truncate the contents to the given length" do
-      page.should render('<r:truncate_and_strip length="10">abcde fghij klmno</r:truncate_and_strip>').as('abcde f...')
+      pages(:search).should render('<r:truncate_and_strip length="10">abcde fghij klmno</r:truncate_and_strip>').as('abcde f...')
     end
   end
 end
