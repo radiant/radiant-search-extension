472f675b1ba31146cf16eb5ff0b091970d5223b7
Jim Gay <jim@saturnflyer.com>
2010-10-01 02:09:54 -0400

    update url

--- search_extension.rb
+++ search_extension.rb
@@ -2,7 +2,7 @@ class SearchExtension < Radiant::Extension
   version "0.1"
   description %{Provides a page type that allows you to search for pages in 
                 Radiant.  Based on Oliver Baltzer's search_behavior.}
-  url "http://dev.radiantcms.org/"
+  url "http://github.com/radiant/radiant-search-extension"
  
   def activate
     SearchPage
