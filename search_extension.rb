9e297c0b6244e9907c011ade451a965ebdbe8f4f
john muhl <git@johnmuhl.com>
2012-01-08 22:50:34 -0600

    update displayed version

--- search_extension.rb
+++ search_extension.rb
@@ -1,14 +1,14 @@
 class SearchExtension < Radiant::Extension
-  version "0.1"
-  description %{Provides a page type that allows you to search for pages in 
+  version "1.0.0"
+  description %{Provides a page type that allows you to search for pages in
                 Radiant.  Based on Oliver Baltzer's search_behavior.}
   url "http://github.com/radiant/radiant-search-extension"
- 
+
   def activate
     SearchPage
   end
-  
+
   def deactivate
   end
-    
+
 end
